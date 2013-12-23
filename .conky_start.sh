321d2967219298ba571fa6e7022cbb0cb8988d5f
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2013-12-23 14:35:19 +0100

    Fedora lua + processes

--- .conky_start.sh
+++ .conky_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+killall conky
 sleep 20
-conky
+conky -c ~/.conkyrc & 
+conky -p 6 -c ~/.conky/conkyconfig/configs/conky_cpu &
+conky -p 8 -c ~/.conky/conkyconfig/configs/conky_ram
+exit
